b448083ccc03adbce991a0347787111f22253983
Robert Bolt <robertbolt@roberts-macbook-air.local>
2018-09-30 19:54:03 -0500

    Initial Commit

---
+++ Parse Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parse Chat
+//
+//  Created by Robert Bolt on 9/30/18.
+//  Copyright © 2018 Robert Bolt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
